ad07d89a7638b5d291276ae6687a82917afaceb5
wangsimiao <wangsimiao.tjpu@bytedance.com>
2021-04-04 20:03:47 +0800

    feat: 第一个组件 Button done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button btnType='primary'>Hello</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
