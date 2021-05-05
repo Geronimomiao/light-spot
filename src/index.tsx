e3a5cbb6b388fcb3a41196483af30ec59be2a83b
wangsimiao <wangsimiao.tjpu@bytedance.com>
2021-05-05 16:09:21 +0800

    feat: 初版发布

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+export { default as Button } from './components/Button'
+export { default as AutoComplete } from './components/AutoComplete'
+export { default as Icon } from './components/Icon'
+export { default as Input } from './components/Input'
+export { default as Progress } from './components/Progress'
+export { default as Transition } from './components/Transition'
+export { default as Image } from './components/Image'
